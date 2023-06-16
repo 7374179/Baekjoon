77499119f41939419f3cab1e24ec8bf42b73540a
taehyuk <7374179@gmail.com>
2023-06-15 23:23:44 -0700

    Jun_15_2023_fail

--- src/main/java/org/example/a016_1377.java
+++ src/main/java/org/example/a016_1377.java
@@ -4,31 +4,6 @@ import java.util.Scanner;
 
 public class a016_1377 {
 
-//  public static void quickSort(int[] A, int left, int right){
-//    int pl = left;
-//    int pr = right;
-//    int mid = (left+right)/2;
-//    int count = 0;
-//
-//    do {
-//      while (A[pl] < mid) pl++;
-//      while (A[pr] > mid) pr--;
-//      if (pl <= pr) {
-//        int tmp = A[pl];
-//        A[pl] = A[pr];
-//        A[pr] = tmp;
-//        count++;
-//      }
-//      if(count==0){
-//        System.out.println(A[pl]);
-//        break;
-//      }
-//    }while(pl<=pr);
-//
-//      if(left<pr)  quickSort(A, left, pr);
-//      if(pl<right) quickSort(A, pl, right);
-//    }
-
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
@@ -39,18 +14,16 @@ public class a016_1377 {
       A[i]=sc.nextInt();
     }
 
-//    quickSort(A, 0, N-1);
-
     int count=0;
 
     for(int i=0;i<N;i++){
       count=0;
-      for(int j=i+1;j<N-i;j++){
-        if(A[i]>A[j]){
+      for(int j=0;j<N-1;j++){
+        if(A[j]>A[j+1]){
           count++;
-          int tmp = A[i];
-          A[i] = A[j];
-          A[j] = tmp;
+          int tmp = A[j];
+          A[j] = A[j+1];
+          A[j+1] = tmp;
         }
       }
       if(count==0){
