66837afc23c1e8a4a510d3b0da8df8180126ecb5
Taehyuk <7374179@gmail.com>
2023-06-18 23:29:33 -0700

    Jun_18_2023_doing

--- src/main/java/org/example/a026_1260.java
+++ src/main/java/org/example/a026_1260.java
@@ -23,7 +23,55 @@ public class a026_1260 {
     for(int i=0;i<M;i++){
       int x = sc.nextInt();
       int y = sc.nextInt();
+      graphDFS(x, y);
+      graphBFS(x, y);
+    }
 
+    int[] listDFS = new int[N];
+    int[] listBFS = new int[N];
+
+    visitedDFS = new boolean[N+1];
+    visitedBFS = new boolean[N+1];
+
+    for(int i=1;i<=N;i++){
+      if(!visitedDFS[i]){
+        dfs(i);
+        listDFS[i]=i;
+      }
+    }
+    for(int i=1;i<=N;i++){
+      if(!visitedBFS[i]){
+        bfs(i);
+        listBFS[i]=i;
+      }
+    }
+  }
+
+  private static void graphDFS(int x, int y) {
+    listDFS.get(x).add(y);
+    listDFS.get(y).add(x);
+  }
+
+  private static void graphBFS(int x, int y) {
+    listBFS.get(x).add(y);
+    listBFS.get(y).add(x);
+  }
+
+  private static void dfs(int i) {
+    visitedDFS[i]=true;
+    for(int u:listDFS.get(i)){
+      if(!visitedDFS[i]){
+        dfs(u);
+      }
+    }
+  }
+
+  private static void bfs(int i) {
+    visitedBFS[i]=true;
+    for(int u:listBFS.get(i)){
+      if(!visitedBFS[i]){
+        bfs(u);
+      }
     }
   }
 
