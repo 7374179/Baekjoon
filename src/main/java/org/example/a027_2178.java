2d0e3454f77f22bd194887afac67a22db2f0a967
Taehyuk <7374179@gmail.com>
2023-06-19 00:23:43 -0700

    Jun_18_2023_fail

---
+++ src/main/java/org/example/a027_2178.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class a027_2178 {
+}
---
+++ src/main/java/org/example/a028_1167.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class a028_1167 {
+  private static List<List<Integer>> list = new ArrayList<>();
+  private static boolean[] visited;
+  public static void main(String[] args){
+
+  }
+
+}
