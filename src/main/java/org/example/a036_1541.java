ea5b7fe4d96d50e08063661a14db2bf81e183376
Taehyuk <7374179@gmail.com>
2023-06-21 16:11:52 -0700

    Jun_21_2023_fail_greed

--- src/main/java/org/example/a036_1541.java
+++ src/main/java/org/example/a036_1541.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class a036_1541 {
+  public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+    String tmp = sc.next();
+
+    String[] parts = tmp.split("[+-]");
+
+    int sum = Integer.parseInt(parts[0]);
+
+    for(int i=1;i<parts.length;i++){
+      sum = sum-Integer.parseInt(parts[i]);
+    }
+
+    System.out.println(sum);
+  }
 }
