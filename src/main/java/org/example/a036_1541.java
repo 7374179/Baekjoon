3e90321eb83ba4b5c47eae8e9c1638200bd4f2b9
Taehyuk <7374179@gmail.com>
2023-06-21 17:52:39 -0700

    Jun_21_2023_fail_greed

--- src/main/java/org/example/a036_1541.java
+++ src/main/java/org/example/a036_1541.java
@@ -3,19 +3,30 @@ package org.example;
 import java.util.Scanner;
 
 public class a036_1541 {
+  public static int sum=0;
   public static void main(String[] args){
 
     Scanner sc = new Scanner(System.in);
     String tmp = sc.next();
 
-    String[] parts = tmp.split("[+-]");
+    String[] parts = tmp.split("[-]");
 
-    int sum = Integer.parseInt(parts[0]);
+    sum = Integer.parseInt(parts[0]);
+    split(tmp);
 
     for(int i=1;i<parts.length;i++){
       sum = sum-Integer.parseInt(parts[i]);
     }
 
     System.out.println(sum);
+  }
+
+  private static void split(String tmp) {
+    String[] parts = tmp.split("[+]");
+
+    for(int i=0;i<tmp.length();i++){
+      sum+=Integer.parseInt(parts[i]);
+    }
+
   }
 }
