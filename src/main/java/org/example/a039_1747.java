20f2ab0d67bce7cd68c6094197368f55c4253581
Taehyuk <7374179@gmail.com>
2023-06-23 00:41:07 -0700

    Jun_22_2023_fail_primeNumber

--- src/main/java/org/example/a039_1747.java
+++ src/main/java/org/example/a039_1747.java
@@ -12,7 +12,7 @@ public class a039_1747 {
     arr[ptr++]=2;
     arr[ptr++]=3;
 
-    for(int i=5;i<=N;i+=2){
+    for(int i=5;i<=1000000;i+=2){
       boolean flag = false;
       for(int n=0;arr[n]*arr[n]<=i;n++) {
         if (i % arr[n] == 0) {
@@ -26,21 +26,22 @@ public class a039_1747 {
 
     }
     int sum=0;
-    int count=0;
+    int min=0;
 
     for(int i=0;i<arr.length;i++){
       int tmp = arr[i];
-      int co=0;
+      int co=tmp;
+
       while(tmp>0){
-        co = tmp;
         sum = sum*10+tmp%10;
         tmp=tmp/10;
       }
-      if(sum==co){
-        count++;
+      if(sum==co && sum>=N){
+        min=sum;
+        break;
       }
       sum=0;
     }
-    System.out.println(count);
+    System.out.println(min);
   }
 }
