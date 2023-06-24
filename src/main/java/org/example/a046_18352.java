d6b823c2d00113012fc4cc1ddae3e91368927880
Taehyuk <7374179@gmail.com>
2023-06-24 16:28:09 -0700

    Jun_24_2023_doing_graph

---
+++ src/main/java/org/example/a046_18352.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class a046_18352 {
+  public static void main(String[] args){
+    List<List<Integer>> list = new ArrayList<List<Integer>>();
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+    int K = sc.nextInt();
+    int X = sc.nextInt();
+
+    for(int i=0;i<N;i++){
+      list.add(new ArrayList<>());
+    }
+
+    for(int i=0;i<N;i++){
+
+    }
+
+  }
+}
