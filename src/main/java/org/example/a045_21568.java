a1d91d9553f7ab6fdb7f9af5e2e82c42be152133
taehyuk <7374179@gmail.com>
2023-06-23 21:34:08 -0700

    Jun_23_2023_doing

--- src/main/java/org/example/a045_21568.java
+++ src/main/java/org/example/a045_21568.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class a045_21568 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    int C = sc.nextInt();
+
+
+    for(int i=-100;i<100;i++){
+      for(int j=-100;j<100;j++){
+        if(j == (C-A*i)%B){
+          System.out.println(i+ " "+j);
+          break;
+        }else{
+          System.out.println(-1);
+        }
+      }
+    }
+  }
 }
