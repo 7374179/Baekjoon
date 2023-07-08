80c954834a8cd267d65849c6bdf9b92d29aa74aa
taehyukchung <7374179@gmail.com>
2023-07-07 21:47:23 -0700

    Jul_07_doing_dynamicProgramming

--- src/main/java/org/example/a090_9252.java
+++ src/main/java/org/example/a090_9252.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class a090_9252 {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        int top = 0;
+        int bottom = 0;
+        String tmp = "";
+    }
 }
