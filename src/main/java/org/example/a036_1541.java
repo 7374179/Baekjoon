e312815c186e94f9c32385a66e141ae0ac1b5b3b
taehyukchung <7374179@gmail.com>
2023-07-20 16:22:29 -0700

    Jul_20_pass_Greedy

--- src/main/java/org/example/a036_1541.java
+++ src/main/java/org/example/a036_1541.java
@@ -5,27 +5,26 @@ import java.util.Scanner;
 public class a036_1541 {
   public static int sum=0;
   public static void main(String[] args){
-
     Scanner sc = new Scanner(System.in);
     String tmp = sc.next();
 
-    String[] parts = tmp.split("[-]");
-
-    sum = Integer.parseInt(parts[0]);
     split(tmp);
-
-    for(int i=1;i<parts.length;i++){
-      sum = sum-Integer.parseInt(parts[i]);
-    }
 
     System.out.println(sum);
   }
 
   private static void split(String tmp) {
-    String[] parts = tmp.split("[+]");
+    String[] parts = tmp.split("[-]");
 
-    for(int i=0;i<tmp.length();i++){
-      sum+=Integer.parseInt(parts[i]);
+    for(int i=0;i<parts.length;i++){
+      String[] arr = parts[i].split("[+]");
+      for(int j=0;j<arr.length;j++){
+        if(i==0){
+          sum+=Integer.parseInt(arr[j]);
+        }else{
+          sum-=Integer.parseInt(arr[j]);
+        }
+      }
     }
 
   }
