f39e1f74aa44bcbd0dff1b0ade30c12d80f24559
taehyukchung <7374179@gmail.com>
2023-08-07 23:27:18 -0700

    Aug_7_practice_graph

--- src/main/java/org/example/a056_1753.java
+++ src/main/java/org/example/a056_1753.java
@@ -1,44 +1,73 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class a056_1753 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int V = sc.nextInt();
-        int E = sc.nextInt();
-
-        List<List<Integer>> list = new ArrayList<>();
-
-        for(int i=0;i<V;i++){
-            list.add(new ArrayList<>());
-        }
-
-        int[] arr = new int[V+1];
-        int[] arrT = new int[V+1];
-
-        int K = sc.nextInt();
-        for(int i=1;i<=V;i++){
-           if(i==K){
-               arr[i]=0;
-           }else{
-               arr[i]=Integer.MAX_VALUE;
-           }
+    public static int V, E, K;
+    public static int distance[];
+    public static boolean visited[];
+    public static ArrayList<Edge> list[];
+    public static PriorityQueue<Edge> q = new PriorityQueue<Edge>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
+        distance = new int[V+1];
+        visited = new boolean[V+1];
+        list = new ArrayList[V+1];
+        for(int i=1;i<=V;i++)
+            list[i]=new ArrayList<Edge>();
+        for(int i=0;i<=V;i++) {
+            distance[i] = Integer.MAX_VALUE;
         }
         for(int i=0;i<E;i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            list.get(a).add(b);
-            list.get(a).add(c);
-
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            list[u].add(new Edge(v, w));
         }
-
-
-
-
-
+        q.add(new Edge(K, 0));
+        distance[K]=0;
+        while(!q.isEmpty()){
+            Edge current = q.poll();
+            int c_v=current.vertex;
+            if(visited[c_v]) {
+                continue;
+            }
+            visited[c_v]=true;
+            for(int i=0;i<list[c_v].size();i++){
+                Edge tmp = list[c_v].get(i);
+                int next = tmp.vertex;
+                int value = tmp.value;
+                if(distance[next] > distance[c_v]+value){
+                    distance[next] = value + distance[c_v];
+                    q.add(new Edge(next, distance[next]));
+                }
+            }
+        }
+        for(int i=1;i<=V;i++){
+            if(visited[i])
+                System.out.println(distance[i]);
+            else
+                System.out.println("INF");
+        }
+    }
+}
+class Edge implements Comparable<Edge>{
+    int vertex, value;
+    Edge(int vertex, int value){
+        this.vertex = vertex;
+        this.value = value;
+    }
+    public int compareTo(Edge e){
+        if(this.value > e.value) return 1;
+        else return -1;
     }
 }
