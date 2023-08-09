b8d19c7821ae73feefa7fa52380cfe616de901a4
taehyukchung <7374179@gmail.com>
2023-08-08 17:14:04 -0700

    Aug_8_practice_graph

--- src/main/java/org/example/a059_11657.java
+++ src/main/java/org/example/a059_11657.java
@@ -1,48 +1,63 @@
 package org.example;
 
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class a059_11657 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-
-        int[] arr = new int[N+1];
-
-        for(int i=1;i<=N;i++){
-            arr[i]=Integer.MAX_VALUE;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N, M;
+    static long distance[];
+    static Edge1 edges[];
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        edges = new Edge1[M+1];
+        distance = new long[N+1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int time = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge1(start, end, time);
         }
-        arr[1]=0;
-
-        for(int i=1;i<=M;i++){
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            int C = sc.nextInt();
-            if(arr[A]!=Integer.MAX_VALUE && arr[B]>arr[A]+C){
-                arr[B]=arr[A]+C;
-            }
-        }
-
-        boolean flag = true;
-
-        for(int j=2;j<=N;j++){
-            if(arr[j]==0 || arr[j]<0){
-                flag=false;
-            }
-        }
-
-        for(int i=2;i<=N;i++){
-            if(flag){
-                if(arr[i]==Integer.MAX_VALUE){
-                    System.out.println(-1);
-                }else{
-                    System.out.println(arr[i]);
+        distance[1] = 0;
+        for(int i=1;i<N;i++){
+            for(int j=0;j<M;j++){
+                Edge1 edge = edges[j];
+                if(distance[edge.start]!=Integer.MAX_VALUE && distance[edge.end] > distance[edge.start]+edge.time){
+                    distance[edge.end] = distance[edge.start] + edge.time;
                 }
-            }else{
-                System.out.println(-1);
-                break;
             }
         }
+        boolean mCycle = false;
+        for(int i=0;i<M;i++){
+            Edge1 edge = edges[i];
+            if(distance[edge.start]!=Integer.MAX_VALUE && distance[edge.end] > distance[edge.start]+edge.time){
+                mCycle = true;
+            }
+        }
+        if(!mCycle){
+            for(int i=2;i<=N;i++){
+                if(distance[i]==Integer.MAX_VALUE)
+                    System.out.println("-1");
+                else
+                    System.out.println(distance[i]);
+            }
+        }else{
+            System.out.println("-1");
+        }
+    }
+}
+class Edge1{
+    int start, end, time;
+    public Edge1(int start, int end, int time){
+        this.start = start;
+        this.end = end;
+        this.time = time;
     }
 }
