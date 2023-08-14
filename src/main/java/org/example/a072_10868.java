785f4752510e7a9d9f902e614a8cc805029c69ec
7374179 <7374179@gmail.com>
2023-08-14 13:59:17 -0700

    Aug_14_practice_tree

--- src/main/java/org/example/a072_10868.java
+++ src/main/java/org/example/a072_10868.java
@@ -1,42 +1,67 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class a072_10868 {
     static long[] tree;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         int treeHeight = 0;
         int length = N;
         while(length!=0){
-            length = length/2;
+            length/=2;
             treeHeight++;
         }
-        int treeSize = (int)Math.pow(2, treeHeight+1);
+        int treeSize=(int)Math.pow(2, treeHeight+1);
         int leftNodeStartIndex = treeSize/2-1;
-        tree = new long[treeSize+1];
-
+        tree=new long[treeSize+1];
+        for(int i=0;i<tree.length;i++){
+            tree[i]=Integer.MAX_VALUE;
+        }
         for(int i=leftNodeStartIndex+1;i<=leftNodeStartIndex+N;i++){
-            tree[i]=sc.nextInt();
+            tree[i]=Long.parseLong(br.readLine());
         }
+        setTree(treeSize-1);
         for(int i=0;i<M;i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(findMin(a+leftNodeStartIndex, b+leftNodeStartIndex));
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            s = s + leftNodeStartIndex;
+            e = e + leftNodeStartIndex;
+            System.out.println(getMin(s, e));
         }
+        br.close();
     }
-    private static int findMin(int a, int b){
-        int min = Integer.MAX_VALUE;
-        while(a<=b){
-            int tmp1 = (int) Math.min(tree[a], tree[a+1]);
-            min = (int) Math.min(min, tmp1);
-            int tmp2 = (int) Math.min(tree[b], tree[b-1]);
-            min = (int) Math.min(min, tmp2);
-            a++;
-            b--;
+
+    private static long getMin(int s, int e){
+        long Min = Long.MAX_VALUE;
+        while(s<=e){
+            if(s%2==1){
+                Min = Math.min(Min, tree[s]);
+                s++;
+            }
+            s=s/2;
+            if(e%2==0){
+                Min = Math.min(Min, tree[e]);
+                e--;
+            }
+            e=e/2;
         }
-        return min;
+        return Min;
+    }
+
+    private static void setTree(int i){
+        while(i!=1){
+            if(tree[i/2]>tree[i])
+                tree[i/2]=tree[i];
+            i--;
+        }
     }
 }
