cec77a29a3f189b4024e3ea7e109c404fda24208
7374179 <7374179@gmail.com>
2023-08-14 11:48:35 -0700

    Aug_14_practice_tree

--- src/main/java/org/example/a069_14425.java
+++ src/main/java/org/example/a069_14425.java
@@ -1,29 +1,46 @@
 package org.example;
 
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class a069_14425 {
     public static void main(String[] args) {
-        Set<String> set = new HashSet<>();
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-
-        for (int i = 0; i < N; i++) {
-            String str = sc.next();
-            set.add(str);
-        }
-
-        int count=0;
-
-        for(int i=0;i<M;i++){
-            String str = sc.next();
-            if(set.contains(str)){
-                count++;
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        tNode root = new tNode();
+        while(n>0){
+            String text = sc.next();
+            tNode now = root;
+            for(int i=0;i<text.length();i++){
+                char c = text.charAt(i);
+                if(now.next[c-'a'] == null){
+                    now.next[c-'a'] = new tNode();
+                }
+                now = now.next[c-'a'];
+                if(i == text.length() - 1)
+                    now.isEnd= true;
             }
+            n--;
+        }
+        int count =0;
+        while(m>0){
+            String text = sc.next();
+            tNode now = root;
+            for(int i=0;i<text.length();i++){
+                char c = text.charAt(i);
+                if(now.next[c-'a']==null){
+                    break;
+                }
+                now = now.next[c-'a'];
+                if(i==text.length()-1 && now.isEnd)
+                    count++;
+            }
+            m--;
         }
         System.out.println(count);
     }
+}
+class tNode{
+    tNode[] next = new tNode[26];
+    boolean isEnd;
 }
