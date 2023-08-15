3f85f9ea0470d08bc0e3d137a80748a0b0c52a7a
7374179 <7374179@gmail.com>
2023-08-15 14:02:05 -0700

    Aug_15_pass_dp

--- src/main/java/org/example/a087_11726.java
+++ src/main/java/org/example/a087_11726.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class a087_11726 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long[] arr = new long[1001];
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i=3;i<=N;i++){
+            arr[i]=(arr[i-1]+arr[i-2])%10007;
+        }
+        System.out.println(arr[N]);
+    }
 }
