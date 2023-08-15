4d057edd1edf444165f5511daf538ed9f2b1af37
7374179 <7374179@gmail.com>
2023-08-15 13:39:36 -0700

    Aug_15_practice_dp

--- src/main/java/org/example/a086_2193.java
+++ src/main/java/org/example/a086_2193.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class a086_2193 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long D[][] = new long[N+1][N+2];
+        D[1][1]=1;
+        D[1][0]=0;
+        for(int i=2;i<=N;i++){
+            D[i][0]=D[i-1][1]+D[i-1][0];
+            D[i][1]=D[i-1][0];
+        }
+        System.out.println(D[N][0]+D[N][1]);
+    }
 }
