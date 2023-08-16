f379876977722353b3338de0e60b423d48b56c18
7374179 <7374179@gmail.com>
2023-08-16 11:06:26 -0700

    Aug_16_practice_dp

--- src/main/java/org/example/a094_11049.java
+++ src/main/java/org/example/a094_11049.java
@@ -1,22 +1,47 @@
 package org.example;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class a094_11049 {
-    public static void main(String[] args){
+    static int N;
+    static Matrix[] M;
+    static int[][] D;
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[][] arr = new int[501][501];
-        int min = Integer.MAX_VALUE;
-
-        for(int i=0;i<N;i++){
-            int r = sc.nextInt();
-            int c = sc.nextInt();
-            arr[r][c]=r*c;
+        N = sc.nextInt();
+        M = new Matrix[N+1];
+        D = new int[N+1][N+1];
+        for(int i=0;i<D.length;i++){
+            for(int j=0;j<D[i].length;j++){
+                D[i][j] = -1;
+            }
         }
-
-
-
-
+        for(int i=1;i<=N;i++){
+            int y = sc.nextInt();
+            int x = sc.nextInt();
+            M[i] = new Matrix(y, x);
+        }
+        System.out.println(excute(1, N));
+    }
+    static int excute(int s, int e){
+        int result = Integer.MAX_VALUE;
+        if(D[s][e]!=-1)
+            return D[s][e];
+        if(s==e)
+            return 0;
+        if(s+1==e)
+            return M[s].y*M[s].x*M[e].x;
+        for(int i=s;i<e;i++)
+            result = Math.min(result, M[s].y*M[i].x*M[e].x+excute(s, i) + excute(i+1, e));
+        return D[s][e] = result;
+    }
+    static class Matrix{
+        private int y;
+        private int x;
+        Matrix(int y, int x){
+            this.y = y;
+            this.x = x;
+        }
     }
 }
