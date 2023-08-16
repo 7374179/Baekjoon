ddaf522c4e9d3af667ae17dc1e8422ebfc598353
7374179 <7374179@gmail.com>
2023-08-16 09:27:31 -0700

    Aug_15_practice_dp

--- src/main/java/org/example/a091_1915.java
+++ src/main/java/org/example/a091_1915.java
@@ -4,24 +4,23 @@ import java.util.Scanner;
 
 public class a091_1915 {
     public static void main(String[] args){
+        long[][] D = new long[1001][100];
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-
-        int[][] arr = new int[n][m];
-
+        long max = 0;
         for(int i=0;i<n;i++){
+            String mline = sc.next();
             for(int j=0;j<m;j++){
-                String str = sc.next();
-                arr[i][j]=str.charAt(i)-'0';
+                D[i][j]=Long.parseLong(String.valueOf(mline.charAt(j)));
+                if(D[i][j]==1&&j>0&&i>0){
+                    D[i][j]=Math.min(D[i-1][j-1], Math.min(D[i-1][j], D[i][j-1])) +D[i][j];
+                }
+                if(max<D[i][j]){
+                    max=D[i][j];
+                }
             }
         }
-
-        int count = 0;
-        int max = Integer.MIN_VALUE;
-
-        int i=0;
-        int j=0;
-
+        System.out.println(max*max);
     }
 }
