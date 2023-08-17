a3ac6007a55cb2e05e31a104eb2c042aea79bb68
7374179 <7374179@gmail.com>
2023-08-17 14:11:15 -0700

    Aug_17_practice_sum

--- src/main/java/org/example/a008_1253.java
+++ src/main/java/org/example/a008_1253.java
@@ -9,69 +9,40 @@ import java.util.StringTokenizer;
 
 public class a008_1253 {
   public static void main(String[] args) throws IOException {
-//    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-//    int N = Integer.parseInt(bf.readLine());
-//    long[] arr = new long[N];
-//    int count = 0;
-//
-//    StringTokenizer st = new StringTokenizer(bf.readLine());
-//    for(int i=0;i<N;i++){
-//      arr[i]=Integer.parseInt(st.nextToken());
-//    }
-//
-//    Arrays.sort(arr);
-//
-//
-//
-//    for(int i=2;i<N;i++){
-//      int sr = 0;
-//      int ed = i-1;
-//      while(sr<ed){
-//        if(arr[sr]+arr[ed]==arr[i]){
-//          count++;
-//          break;
-//        }else if(arr[sr]+arr[ed]>arr[i]){
-//          ed--;
-//        }else{
-//          sr++;
-//        }
-//
-//      }
-//    }
-//    if(N==1||N==2){
-//      System.out.println(0);
-//    }else{
-//      System.out.println(count);
-//    }
-//    bf.close();
-
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    int[] arr = new int[N];
-    int[] checked = new int[N];
-
+//    Scanner sc = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    int N = sc.nextInt();
+    int N = Integer.parseInt(br.readLine());
+    long[] A = new long[N];
+    StringTokenizer st = new StringTokenizer(br.readLine());
     for(int i=0;i<N;i++){
-      arr[i]=sc.nextInt();
+      A[i] = Long.parseLong(st.nextToken());
+//      A[i] = sc.nextLong();
     }
+    Arrays.sort(A);
 
-    int left=0;
-    int curr=1;
-    int sum=0;
-    int tmp=left+curr;
+    int left = 0;
+    int curr = 2;
+    int right = curr-1;
+    int count = 0;
 
-    while(left<N){
-      sum=arr[left]+arr[curr];
-      while(tmp<N){
-        if(sum==arr[tmp]){
-          checked[arr[tmp]]++;
-          curr++;
-          break;
-        }else{
-          tmp++;
-        }
+    while(curr <A.length){
+      if(A[curr]==A[left]+A[right]){
+        count++;
+        curr++;
+        right = curr-1;
+        left=0;
+      }else if(A[curr]>A[left]+A[right]){
+        left++;
+      }else if(A[curr]<A[left]+A[right]){
+        right--;
+      }
+      if(left>=right){
+        curr++;
+        left=0;
+        right=curr-1;
       }
     }
-
-
+    System.out.println(count);
   }
 }
