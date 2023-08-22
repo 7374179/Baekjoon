cc50df0412222d6e20efeebeb9733c18820a9ba4
7374179 <7374179@gmail.com>
2023-08-22 10:54:31 -0700

    Aug_22_practice_stack

--- src/main/java/org/example/a011_1874.java
+++ src/main/java/org/example/a011_1874.java
@@ -4,75 +4,38 @@ import java.util.*;
 
 public class a011_1874 {
   public static void main(String[] args){
-//    Stack<Integer> stack = new Stack<>();
-//    List<String> list = new ArrayList<>();
-//    Scanner sc = new Scanner(System.in);
-//    int n = sc.nextInt();
-//
-//    int[] arr = new int[n];
-//
-//    for(int i=0;i<n;i++){
-//      arr[i]=sc.nextInt();
-//    }
-//
-//    int i = 0;
-//    int j = 1;
-//
-//    while(i<n){
-//      while(i<n){
-//        if(arr[i]>=j){
-//          stack.push(j);
-//          list.add("+");
-//        }else if(arr[i]<j){
-//          if(stack.peek()==arr[i]){
-//            stack.pop();
-//            list.add("-");
-//            break;
-//          }else{
-//            list.add("NO");
-//            break;
-//          }
-//        }else{
-//          stack.pop();
-//          list.add("-");
-//        }
-//        j++;
-//      }
-//      i++;
-//    }
-//
-//    Iterator it = list.iterator();
-//    while(it.hasNext()){
-//      if(list.contains("NO")){
-//        System.out.println("NO");
-//        break;
-//      }else{
-//        System.out.println(it.next());
-//      }
-//    }
-
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    Stack<Integer> stack = new Stack<>();
-    int[] arr = new int[n+1];
-    for(int i=0;i<n;i++){
-      int a = sc.nextInt();
-      arr[i]=i;
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for(int i=0;i<N;i++){
+      A[i] = sc.nextInt();
     }
-    int j=1;
-
-    for(int i=0;i<n;i++){
-      while(j<n){
-        if(stack.peek()==j){
-          System.out.println("-");
-          stack.pop();
-          j--;
-        }else if(stack.peek()>j){
-          System.out.println("+");
-          stack.add(j);
-          j++;
+    Stack<Integer> stack = new Stack<>();
+    StringBuffer bf = new StringBuffer();
+    int num = 1;
+    boolean result = true;
+    for(int i=0;i<A.length;i++){
+      int su = A[i];
+      if (su >= num){
+        while(su>=num){
+          stack.push(num++);
+          bf.append("+\n");
+        }
+        stack.pop();
+        bf.append("-\n");
+      } else{
+        int n = stack.pop();
+        if(n>su){
+          System.out.println("NO");
+          result = false;
+          break;
+        }else{
+          bf.append("-\n");
         }
       }
+    }
+    if(result){
+      System.out.println(bf.toString());
     }
   }
 }
