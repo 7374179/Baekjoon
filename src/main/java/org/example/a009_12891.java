0e21f7f0af9dd493de1297fe7d4758237bc1f767
7374179 <7374179@gmail.com>
2023-08-22 10:54:07 -0700

    Aug_17_practice_sum

--- src/main/java/org/example/a009_12891.java
+++ src/main/java/org/example/a009_12891.java
@@ -6,54 +6,89 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class a009_12891 {
-  public static void main(String[] args) throws IOException {
-    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(bf.readLine());
+  static int checkArr[];
+  static int myArr[];
+  static int checkSecret;
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
     int S = Integer.parseInt(st.nextToken());
     int P = Integer.parseInt(st.nextToken());
-
-    String str = bf.readLine();
-    char[] ch = str.toCharArray();
-
-    int A=0;
-    int C=0;
-    int G=0;
-    int T=0;
-
-    st = new StringTokenizer(bf.readLine());
-    A = Integer.parseInt(st.nextToken());
-    C = Integer.parseInt(st.nextToken());
-    G = Integer.parseInt(st.nextToken());
-    T = Integer.parseInt(st.nextToken());
-
-    int num = A+C+G+T;
-
-    char[] nc = new char[num];
-    int i=0;
-    while(i<num){
-      if(A>0){
-        nc[i]='A';
-        A--;
-        i++;
-      }
-      if(C>0){
-        nc[i]='C';
-        C--;
-        i++;
-      }
-      if(G>0){
-        nc[i]='G';
-        G--;
-        i++;
-      }
-      if(T>0){
-        nc[i]='T';
-        T--;
-        i++;
-      }
+    int Result = 0;
+    char A[] = new char[S];
+    checkArr = new int[4];
+    myArr = new int[4];
+    checkSecret = 0;
+    A = br.readLine().toCharArray();
+    st = new StringTokenizer(br.readLine());
+    for(int i=0;i<4;i++){
+      checkArr[i] = Integer.parseInt(st.nextToken());
+      if(checkArr[i]==0)
+        checkSecret++;
     }
+    for(int i=0;i<P;i++){
+      Add(A[i]);
+    }
+    if(checkSecret==4)
+      Result++;
+    for(int i=P;i<S;i++){
+      int j = i-P;
+      Add(A[i]);
+      Remove(A[j]);
+      if(checkSecret == 4)
+        Result++;
+    }
+    System.out.println(Result);
+    br.close();
+  }
 
+  private static void Add(char c){
+    switch(c){
+      case 'A':
+        myArr[0]++;
+        if(myArr[0] == checkArr[0])
+          checkSecret++;
+        break;
+      case 'C':
+        myArr[1]++;
+        if(myArr[1] == checkArr[1])
+          checkSecret++;
+        break;
+      case 'G':
+        myArr[2]++;
+        if(myArr[2] == checkArr[2])
+          checkSecret++;
+        break;
+      case 'T':
+        myArr[3]++;
+        if(myArr[3] == checkArr[3])
+          checkSecret++;
+        break;
+    }
+  }
 
-
+  private static void Remove(char c){
+    switch (c){
+      case 'A':
+        if(myArr[0] == checkArr[0])
+          checkSecret--;
+        myArr[0]--;
+        break;
+      case 'C':
+        if(myArr[1] == checkArr[1])
+          checkSecret--;
+        myArr[1]--;
+        break;
+      case 'G':
+        if(myArr[2] == checkArr[2])
+          checkSecret--;
+        myArr[2]--;
+        break;
+      case 'T':
+        if(myArr[3] == checkArr[3])
+          checkSecret--;
+        myArr[3]--;
+        break;
+    }
   }
 }
