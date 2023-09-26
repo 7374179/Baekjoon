611a2a24191c6ac6946998d0b597e1e1dcf599cd
7374179 <7374179@gmail.com>
2023-09-26 08:36:24 -0700

    Sep_26_practice_greedy

--- src/main/java/org/example/a033_1715.java
+++ src/main/java/org/example/a033_1715.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class a033_1715 {
@@ -7,19 +8,20 @@ public class a033_1715 {
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
     int[] arr = new int[N];
-    int sum=0;
-    int min=Integer.MAX_VALUE;
-
+    PriorityQueue<Integer> que = new PriorityQueue<>();
     for(int i=0;i<N;i++){
-      arr[i]=sc.nextInt();
-      sum+=arr[i];
+      que.add(sc.nextInt());
+    }
+    int data1=0;
+    int data2=0;
+    int sum=0;
+    while(que.size()!=1){
+      data1=que.poll();
+      data2=que.poll();
+      sum=sum+data1+data2;
+      que.add(sum);
     }
 
-    for(int i=0;i<N-1;i++){
-      for(int j=i+1;j<N;j++){
-        min=Math.min(min, sum+arr[i]+arr[j]);
-      }
-    }
-    System.out.println(min);
+    System.out.println(sum);
   }
 }
