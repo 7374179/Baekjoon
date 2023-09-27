0e522b9a70ab46dcf7953a00747448cfce1a16fe
7374179 <7374179@gmail.com>
2023-09-27 08:43:52 -0700

    Sep_27_practice_greedy

--- src/main/java/org/example/a034_1744.java
+++ src/main/java/org/example/a034_1744.java
@@ -1,39 +1,50 @@
 package org.example;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class a034_1744 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
-    int[] arr = new int[N];
+    PriorityQueue<Integer> plusPq = new PriorityQueue<>(Collections.reverseOrder());
+    PriorityQueue<Integer> minusPq = new PriorityQueue<>();
+    int one=0;
+    int zero=0;
 
-    for (int i = 0; i < N; i++) {
-      arr[i] = sc.nextInt();
-    }
-    Arrays.sort(arr);
-
-    int sum1 = 0;
-
-    for (int a : arr) {
-      sum1 += a;
-    }
-
-    int sum2 = 0;
-    int max = Integer.MIN_VALUE;
-
-    for (int i = 0; i < N / 2; i++) {
-      if (arr[i] < 0 && arr[i + 1] < 0) {
-        sum2 += arr[i] * arr[i + 1];
-      } else if (arr[i] < 0 && arr[i + 1] > 0) {
-        sum2 += arr[i] + arr[i + 1];
-      } else if (arr[i] < 0 && arr[i + 1] == 0) {
-        sum2 += arr[i] * arr[i + 1];
-      } else if (arr[i] == 0 && arr[i + 1] > 0) {
-        sum2 += arr[i] + arr[i + 1];
+    for(int i=0;i<N;i++){
+      int data=sc.nextInt();
+      if(data>1){
+        plusPq.add(data);
+      } else if(data == 1){
+        one++;
+      } else if(data == 0){
+        zero++;
+      } else{
+        minusPq.add(data);
       }
     }
-
+    int sum = 0;
+    while(plusPq.size()>1){
+      int first = plusPq.remove();
+      int second = plusPq.remove();
+      sum = sum + first * second;
+    }
+    if(!plusPq.isEmpty()){
+      sum=sum + plusPq.remove();
+    }
+    while(minusPq.size()>1){
+      int first = minusPq.remove();
+      int second = minusPq.remove();
+      sum = sum + first * second;
+    }
+    if(!minusPq.isEmpty()){
+      if(zero == 0){
+        sum = sum + minusPq.remove();
+      }
+    }
+    sum = sum + one;
+    System.out.println(sum);
   }
 }
