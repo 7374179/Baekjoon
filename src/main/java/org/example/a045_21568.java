ff4a2e0cf4276e2c5ea3e404e641c1e8914dc74f
7374179 <7374179@gmail.com>
2023-10-06 08:16:24 -0700

    Oct_6_practice_euclidean

--- src/main/java/org/example/a045_21568.java
+++ src/main/java/org/example/a045_21568.java
@@ -1,24 +1,48 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class a045_21568 {
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    int A = sc.nextInt();
-    int B = sc.nextInt();
-    int C = sc.nextInt();
-
-
-    for(int i=-100;i<100;i++){
-      for(int j=-100;j<100;j++){
-        if(j == (C-A*i)%B){
-          System.out.println(i+ " "+j);
-          break;
-        }else{
-          System.out.println(-1);
-        }
-      }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    int c = Integer.parseInt(st.nextToken());
+    long gcd = gcd(a, b);
+    if(c%gcd!=0){
+      System.out.println(-1);
+    } else {
+      int mok = (int)(c/gcd);
+      long[] ret = Excute(a, b);
+      System.out.println(ret[0]*mok + " " + ret[1]*mok);
     }
+  }
+  private static long[] Excute(long a, long b){
+    long[] ret = new long[2];
+    if(b==0){
+      ret[0] = 1;
+      ret[1] = 0;
+      return ret;
+    }
+    long q = a/b;
+    long[] v = Excute(b, a%b);
+    ret[0] = v[1];
+    ret[1] = v[0] - v[1] * q;
+    return ret;
+  }
+  private static long gcd(long a, long b){
+    while(b!=0){
+      long temp = a%b;
+      a=b;
+      b=temp;
+    }
+    return Math.abs(a);
   }
 }
